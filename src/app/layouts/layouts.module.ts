21e3f6ca1c3c1c53ba0fc428e0155d61095ddb9b
Andrew Buvria <buvrya228@gmail.com>
2023-03-05 14:19:15 +0100

    feat: create pages, layouts and shared

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MainComponent } from './main';
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutsModule { }
